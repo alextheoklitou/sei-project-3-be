a2b76e51fb917f8519ddf25211494ebb8af17fcb
Alex Theoklitou <alex_theo_cy@hotmail.com>
2023-02-02 12:15:32 +0000

    redeployment

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const dbURI =
-  process.env.DB_URI || 'mongodb://localhost/sniffs-db'
+export const dbURI = process.env.DB_URI || 'mongodb://localhost/sniffs-db'
 export const port = process.env.PORT || 4000
 export const secret = process.env.SECRET || 'What is small but can climb a tower?'
